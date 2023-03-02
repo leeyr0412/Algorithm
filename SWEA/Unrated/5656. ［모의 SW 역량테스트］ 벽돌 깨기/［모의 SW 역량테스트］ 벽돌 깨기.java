4eeaa39a66a0df4da1bdcfb1a2b48548485a1d89
Yeri Lee <64480162+leeyr0412@users.noreply.github.com>
2023-03-03 00:38:59 +0900

    [Unrated] Title: [모의 SW 역량테스트] 벽돌 깨기, Time: 1,073 ms, Memory: 88,672 KB -BaekjoonHub

--- SWEA/Unrated/5656. ［모의 SW 역량테스트］ 벽돌 깨기/［모의 SW 역량테스트］ 벽돌 깨기.java
+++ SWEA/Unrated/5656. ［모의 SW 역량테스트］ 벽돌 깨기/［모의 SW 역량테스트］ 벽돌 깨기.java
@@ -6,10 +6,9 @@ import java.util.Deque;
 import java.util.StringTokenizer;
 
 /**
- * 벽돌 깨기 /  / / 3월 2일
+ * 벽돌 깨기 / 1시간 반
  */
 public class Solution {
-    //    static ArrayList<Integer[]> permList = new ArrayList<>();
     static int H;
     static int W;
     static int answer = Integer.MAX_VALUE;
@@ -23,8 +22,7 @@ public class Solution {
 
         int T = Integer.parseInt(br.readLine());
         for (int tc = 1; tc <= T; tc++) {
-//            answer = 0;
-            answer = Integer.MAX_VALUE;
+            answer = Integer.MAX_VALUE; //초기화 및 입력받기
             st = new StringTokenizer(br.readLine());
             int balls = Integer.parseInt(st.nextToken());
             W = Integer.parseInt(st.nextToken());
@@ -37,8 +35,9 @@ public class Solution {
                     board[h][w] = Integer.parseInt(st.nextToken());
                     originalBoard[h][w] = board[h][w];
                 }
-                perm = new int[balls];
             }
+            perm = new int[balls];
+            //조합구하기
             permutation(W, balls, 0);
 
             System.out.println("#" + tc + " " + answer);
@@ -49,10 +48,9 @@ public class Solution {
 
     private static void permutation(int w, int n, int cnt) {
         if (cnt == n) {//순열 구해짐
-            gameGo();
-//            fall();
-            answer = Math.min(answer, count());
-            copyBoard();
+            gameGo();   //게임하기
+            answer = Math.min(answer, count()); //남은 블럭 수 세서 최솟값 구하기
+            copyBoard();    //다시 보드 복사
             return;
         }
         for (int i = 0; i < w; i++) {
@@ -62,16 +60,16 @@ public class Solution {
     }
 
     private static void copyBoard() {
-        for(int row = 0; row<H; row++){
+        for (int row = 0; row < H; row++) {
             board[row] = originalBoard[row].clone();
         }
     }
 
-    private static int count() {
+    private static int count() {        //블럭 수 세기
         int count = 0;
-        for(int r = 0; r<H; r++){
-            for(int c = 0; c < W; c++){
-                if(board[r][c]!=0){
+        for (int r = 0; r < H; r++) {
+            for (int c = 0; c < W; c++) {
+                if (board[r][c] != 0) {
                     count++;
                 }
             }
@@ -79,51 +77,37 @@ public class Solution {
         return count;
     }
 
-    private static void gameGo() {
+    private static void gameGo() {      //구해진 순열으로 맨 위 블럭 찾기
         for (int w : perm) {
             for (int h = 0; h < H; h++) {
                 if (board[h][w] != 0) {
-                    shoot(h, w);
-//                    for(int r = 0; r < H; r++){
-//                        for(int c = 0; c < W; c++){
-//                            System.out.printf("%d ", board[r][c]);
-//                        }
-//                        System.out.println();
-//                    }
-//                    System.out.println("================================");
-                    fall();
-//                    for(int r = 0; r < H; r++){
-//                        for(int c = 0; c < W; c++){
-//                            System.out.printf("%d ", board[r][c]);
-//                        }
-//                        System.out.println();
-//                    }
-//                    System.out.println("================================");
+                    shoot(h, w);    //맨 위 블럭부터 없앨수있는 놈 없애기
+                    fall();         //아래로 떨어짐
                     break;
                 }
             }
         }
     }
 
-    private static void fall() {
+    private static void fall() {    //떨어짐
         Deque<Integer> que = new ArrayDeque<>();
-        for(int c = 0; c < W; c++){
-            for(int r = H-1;r>=0;r--){
-                if(board[r][c]>0) {
+        for (int c = 0; c < W; c++) {       //아래부터 큐에 다 넣고
+            for (int r = H - 1; r >= 0; r--) {
+                if (board[r][c] > 0) {
                     que.offer(board[r][c]);
                 }
             }
-            for(int r = H-1;r>=0;r--){
-                if(que.size()>0) {
+            for (int r = H - 1; r >= 0; r--) {  //다시 아래부터 채우기
+                if (que.size() > 0) {
                     board[r][c] = que.poll();
-                }else{
+                } else {
                     board[r][c] = 0;
                 }
             }
         }
     }
 
-    private static void shoot(int r, int c) {
+    private static void shoot(int r, int c) {   //쏘기 bfs
         Deque<int[]> que = new ArrayDeque<>();
         que.offer(new int[]{r, c, board[r][c]});
         board[r][c] = 0;
@@ -134,8 +118,8 @@ public class Solution {
                 for (int[] d : dr) {
                     int newR = pos[0] + d[0] * power;
                     int newC = pos[1] + d[1] * power;
-                    if(newR >= 0 && newR < H && newC >= 0 && newC < W){
-                        if(board[newR][newC]>0){
+                    if (newR >= 0 && newR < H && newC >= 0 && newC < W) {
+                        if (board[newR][newC] > 0) {
                             que.offer(new int[]{newR, newC, board[newR][newC]});
                             board[newR][newC] = 0;
                         }
