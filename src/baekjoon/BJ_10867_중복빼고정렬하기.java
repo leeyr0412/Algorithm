9409810a6536ea0578b8c69013d998f2ac11300c
Yeri Lee <64480162+leeyr0412@users.noreply.github.com>
2023-03-08 23:58:29 +0900

    Update BJ_10867_중복빼고정렬하기.java

--- src/baekjoon/BJ_10867_중복빼고정렬하기.java
+++ src/baekjoon/BJ_10867_중복빼고정렬하기.java
@@ -5,9 +5,6 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
 
-/**
- *
- */
 public class BJ_10867_중복빼고정렬하기 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
