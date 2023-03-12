f860ca44d89f3e05228f250c464733a950ac8bf5
Yeri Lee <64480162+leeyr0412@users.noreply.github.com>
2023-03-12 18:55:05 +0900

    [Silver IV] Title: 큐, Time: 348 ms, Memory: 19160 KB -BaekjoonHub

---
+++ 백준/Silver/10845. 큐/큐.java
@@ -0,0 +1,60 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		
+		int N = Integer.parseInt(br.readLine());
+		Deque<Integer> que = new ArrayDeque<>(); 
+		
+		for(int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine());
+			
+			switch (st.nextToken()) {
+			case "push":
+				que.add(Integer.parseInt(st.nextToken()));
+				break;
+			case "pop":
+				if(que.size()!=0) {
+					System.out.println(que.poll());
+					que.remove(0);
+				}else {
+					System.out.println(-1);
+				}
+				break;
+			case "size":
+				System.out.println(que.size());
+				break;
+			case "empty":
+				if(que.size()!=0) {
+					System.out.println(0);
+				}else {
+					System.out.println(1);
+				}
+				break;
+			case "front":
+				if(que.size()!=0) {
+					System.out.println(que.peekFirst());
+				}else {
+					System.out.println(-1);
+				}
+				break;
+			case "back":
+				if(que.size()!=0) {
+					System.out.println(que.peekLast());
+				}else {
+					System.out.println(-1);
+				}
+				break;
+			}
+		}
+	}
+}
