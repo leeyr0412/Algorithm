2f470fc9f2bdacd0984b67c0869adbce8a365e23
Yeri Lee <64480162+leeyr0412@users.noreply.github.com>
2023-03-22 09:06:23 +0900

    [Gold IV] Title: 운동, Time: 800 ms, Memory: 67332 KB -BaekjoonHub

---
+++ 백준/Gold/1956. 운동/운동.java
@@ -0,0 +1,56 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+/**
+ * 운동 / 골드4 / 걸린시간 / 3월 21일
+ */
+public class Main {
+    private static final int INF = (int) 1e9;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuffer sb = new StringBuffer();
+
+        int v = Integer.parseInt(st.nextToken());
+        int e = Integer.parseInt(st.nextToken());
+        int[][] map = new int[v + 1][v + 1];
+        for (int r = 1; r <= v; r++) {
+            for (int c = 1; c <= v; c++) {
+                if(r!=c){
+                    map[r][c] = INF;
+                }
+            }
+        }
+
+        for (int i = 0; i < e; i++) {
+            st = new StringTokenizer(br.readLine());
+            int from = Integer.parseInt(st.nextToken());
+            int to = Integer.parseInt(st.nextToken());
+            int cost = Integer.parseInt(st.nextToken());
+            map[from][to] = Math.min(cost,map[from][to]);
+        }
+        for (int from = 1; from <= v; from++) {
+            for (int to = 1; to <= v; to++) {
+                if (from == to)
+                    continue;
+                for (int use = 1; use <= v; use++) {
+                    if(use!=from && use!= to) {
+                        map[from][to] = Math.min(map[from][to], map[from][use] + map[use][to]);
+                    }
+                }
+            }
+        }
+        int result = Integer.MAX_VALUE;
+        for (int from = 1; from <= v; from++) {
+            for (int to = 1; to <= v; to++) {
+                if (from == to)
+                    continue;
+                result = Math.min(result, map[from][to] + map[to][from]);
+            }
+        }
+        System.out.println(result < INF ? result : -1);
+    }
+}
