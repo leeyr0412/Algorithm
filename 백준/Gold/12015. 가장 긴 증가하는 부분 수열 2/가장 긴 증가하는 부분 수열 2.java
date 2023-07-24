37469904eb4b046433e2d0d45136897f40cabc55
Yeri Lee <64480162+leeyr0412@users.noreply.github.com>
2023-07-24 14:05:33 +0900

    [Gold II] Title: 가장 긴 증가하는 부분 수열 2, Time: 520 ms, Memory: 96208 KB -BaekjoonHub

---
+++ 백준/Gold/12015. 가장 긴 증가하는 부분 수열 2/가장 긴 증가하는 부분 수열 2.java
@@ -0,0 +1,56 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        //StringBuilder sb = new StringBuilder();
+        StringTokenizer st;// = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(br.readLine());
+        int[] nums = new int[N];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            nums[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int[] list = new int[N];
+        int top = 0;
+
+        list[0] = nums[0];
+        for (int i = 1; i < N; i++) {
+            if (list[top] < nums[i]) {
+                top++;
+                list[top] = nums[i];
+            } else {
+                int index = binarySearch(list, nums[i], top);
+                list[index] = nums[i];
+            }
+        }
+        System.out.println(top+1);
+
+    }
+
+    private static int binarySearch(int[] list, int key, int top) {
+        int start = 0;
+        int end = top;
+        int mid = 0;
+        while (start <= end) {
+            mid = (start + end) >> 1;
+            if (list[mid] == key) {
+                return mid;
+            } else if (list[mid] < key) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+        if(list[mid]>key){
+            return mid;
+        }else {
+            return mid+1;
+        }
+    }
+}
