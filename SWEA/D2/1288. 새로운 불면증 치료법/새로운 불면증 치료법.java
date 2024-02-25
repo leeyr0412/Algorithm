d1bc7ce347e7f891b2b736069da76beb126fc1e0
Yeri Lee <64480162+leeyr0412@users.noreply.github.com>
2024-02-26 01:10:51 +0900

    Update 새로운 불면증 치료법.java

--- SWEA/D2/1288. 새로운 불면증 치료법/새로운 불면증 치료법.java
+++ SWEA/D2/1288. 새로운 불면증 치료법/새로운 불면증 치료법.java
@@ -6,7 +6,6 @@ import java.util.StringTokenizer;
 public class Solution {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-//        StringTokenizer st = new StringTokenizer(br.readLine());
         StringBuilder sb = new StringBuilder();
 
         int T = Integer.parseInt(br.readLine());
