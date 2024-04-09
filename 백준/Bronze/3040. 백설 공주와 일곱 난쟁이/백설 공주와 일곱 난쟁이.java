8e07ab6c5308a406b2ddb4327dc1c16ac969dfee
Yeri Lee <64480162+leeyr0412@users.noreply.github.com>
2024-04-09 17:09:30 +0900

    [Bronze II] Title: 백설 공주와 일곱 난쟁이, Time: 124 ms, Memory: 14164 KB -BaekjoonHub

---
+++ 백준/Bronze/3040. 백설 공주와 일곱 난쟁이/백설 공주와 일곱 난쟁이.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int total = 0;
+        int [] arr = new int[9];
+        for(int i = 0; i < 9; i++){
+            arr[i]= Integer.parseInt(br.readLine());
+            total += arr[i];
+        }
+//        Collections.sort(arr);
+        for(int i = 0; i <8; i++){
+            for(int j = i+1; j<9; j++){
+                int temp = arr[i] + arr[j];
+                if(total - temp == 100){
+                    arr[i] = 0; arr[j] = 0;
+                    for(int index = 0; index < 9; index++){
+                        if(arr[index]!=0)
+                            System.out.println(arr[index]);
+                    }
+                    return;
+                }
+            }
+        }
+    }
+}
