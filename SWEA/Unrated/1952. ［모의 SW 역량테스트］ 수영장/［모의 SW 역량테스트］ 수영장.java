6909365c59ee3f3189c1656ba06999fdcb0729d3
Yeri Lee <64480162+leeyr0412@users.noreply.github.com>
2024-04-25 19:18:53 +0900

    [Unrated] Title: [모의 SW 역량테스트] 수영장, Time: 148 ms, Memory: 22,040 KB -BaekjoonHub

---
+++ SWEA/Unrated/1952. ［모의 SW 역량테스트］ 수영장/［모의 SW 역량테스트］ 수영장.java
@@ -0,0 +1,43 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Solution {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+
+		int T = Integer.parseInt(br.readLine());
+		for(int tc = 1; tc <= T; tc++) {
+			int[] cost = new int [4];
+			int[] uses = new int [12];
+			
+			st = new StringTokenizer(br.readLine());
+			for(int i =0; i <4; i++	) {
+				cost[i] = Integer.parseInt(st.nextToken());
+			}
+			st = new StringTokenizer(br.readLine());
+			for(int i =0; i <12; i++	) {
+				uses[i] = Integer.parseInt(st.nextToken());
+			}
+			
+			int result = cost[3];
+			
+			int [] dp = new int [13];
+			for(int i = 1; i <=12; i++){
+				int temp = Math.min(dp[i-1]+uses[i-1] * cost[0],dp[i-1]+cost[1]);	//일일권이나 1달권 비교
+				if(i >=3)
+					temp = Math.min(temp,dp[i-3]+cost[2]);
+				if(i==12){
+					temp = Math.min(cost[3],temp);
+				}
+				dp[i] = temp;
+			}
+			result = dp[12];
+			
+			System.out.println("#" + tc+" "+ result);
+		}
+	}
+}
