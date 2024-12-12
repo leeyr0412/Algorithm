e4e83a1382687c0e7b0b52018b2d3bdf01635503
Yeri Lee <leeyr0412@naver.com>
2024-12-12 19:18:53 +0900

    [level 4] Title: 지형 이동, Time: 138.11 ms, Memory: 121 MB -BaekjoonHub

--- 프로그래머스/4/62050. 지형 이동/지형 이동.java
+++ 프로그래머스/4/62050. 지형 이동/지형 이동.java
@@ -1,17 +1,17 @@
 import java.util.*;
 
 class Ladder implements Comparable<Ladder> {
-    int size;
+    int cost;
     int[] pos;
 
-    public Ladder(int size, int[] pos) {
-        this.size = size;
+    public Ladder(int cost, int[] pos) {
+        this.cost = cost;
         this.pos = pos;
     }
 
     @Override
     public int compareTo(Ladder o) {
-        return this.size - o.size;
+        return this.cost - o.cost;
     }
 }
 
@@ -21,57 +21,36 @@ class Solution {
     public int solution(int[][] land, int height) {
         int answer = 0;
         boolean[][] visited = new boolean[land.length][land.length];
-        int remainCount = land.length * land.length - 1;    // 방문하지 않은 좌표의 수
+        int remainCount = land.length * land.length;    // 방문하지 않은 좌표의 수
 
-        visited[0][0] = true;
-        // BFS 탐색을 위한 큐
-        Queue<int[]> findQueue = new ArrayDeque<>();
-        // 사다리 큐(사다리 짧은 순으로 정렬)
-        PriorityQueue<Ladder> ladderQueue = new PriorityQueue<>();
-        findQueue.add(new int[]{0, 0});
-        while (remainCount != 0) {
-            // BFS 탐색으로 방문한 좌표 수만큼 남은 좌표수가 빠짐.
-            remainCount = bfs(land, height, visited, remainCount, findQueue, ladderQueue);
-            // 방문하지 않은 남은 좌표가 있다면
-            if (remainCount > 0) {
-                while (true) {
-                    // 사다리 탐색
-                    Ladder ladder = ladderQueue.poll();
-                    if (visited[ladder.pos[0]][ladder.pos[1]]) {    // 사다리 놓을 위치에 방문함
-                        continue;
-                    } else {// 사다리 놓기
-                        visited[ladder.pos[0]][ladder.pos[1]] = true;
-                        findQueue.add(new int[]{ladder.pos[0], ladder.pos[1]});
-                        answer += ladder.size;
-                        remainCount--;
-                        break;
-                    }
+        PriorityQueue<Ladder> queue = new PriorityQueue<>();
+        queue.add(new Ladder(0, new int[]{0, 0}));
+        while (!queue.isEmpty()) {
+            Ladder now = queue.poll();
+            if (visited[now.pos[0]][now.pos[1]]) {    // 방문 한 지점이면 넘어감
+                continue;
+            }
+            visited[now.pos[0]][now.pos[1]] = true;
+            remainCount--;
+            answer += now.cost; // 사다리 가치 더함.(사다리 필요 없으면 0)
+            if (remainCount == 0) {
+                break;
+            }
+            for (int[] dir : dirs) {
+                int r = now.pos[0] + dir[0];
+                int c = now.pos[1] + dir[1];
+                if (isInvalid(r, c, land.length) || visited[r][c]) {
+                    continue;
+                }
+                int diff = Math.abs(land[now.pos[0]][now.pos[1]] - land[r][c]);
+                if (diff <= height) {   // 사다리 길이보다 짧으면 사다리 비용 0
+                    queue.add(new Ladder(0, new int[]{r, c}));
+                } else {
+                    queue.add(new Ladder(diff, new int[]{r, c}));
                 }
             }
         }
         return answer;
-    }
-
-    private int bfs(int[][] land, int height, boolean[][] visited, int remainCount, Queue<int[]> findQueue, PriorityQueue<Ladder> ladderQueue) {
-        while (!findQueue.isEmpty()) {
-            int[] now = findQueue.poll();
-            for (int[] dir : dirs) {
-                int r = now[0] + dir[0];
-                int c = now[1] + dir[1];
-                if (isInvalid(r, c, land.length) || visited[r][c]) {
-                    continue;
-                }
-                int diff = Math.abs(land[now[0]][now[1]] - land[r][c]);
-                if (diff <= height) {
-                    findQueue.add(new int[]{r, c});
-                    visited[r][c] = true;
-                    remainCount--;
-                } else {    // 사다리가 필요할 경우 사다리 큐에 사다리 크기와 방문하지 못한 좌표 넣음
-                    ladderQueue.add(new Ladder(diff, new int[]{r, c}));
-                }
-            }
-        }
-        return remainCount;
     }
 
     private boolean isInvalid(int r, int c, int length) {
