f5a2da3059db09ee67de740c8d59c99016d0ca34
Yeri Lee <leeyr0412@naver.com>
2025-01-07 19:18:53 +0900

    [Gold II] Title: 보석 도둑, Time: 1248 ms, Memory: 119876 KB -BaekjoonHub

--- 백준/Gold/1202. 보석 도둑/보석 도둑.java
+++ 백준/Gold/1202. 보석 도둑/보석 도둑.java
@@ -23,12 +23,12 @@ class Gem implements Comparable<Gem> {
         return cost;
     }
 
+    /**
+     * 무게 기준 오름차순
+     */
     @Override
     public int compareTo(Gem o) {
-        if (o.m != this.m) {
-            return this.m - o.m;
-        }
-        return o.cost - this.cost;
+        return this.m - o.m;
     }
 }
 
@@ -36,11 +36,11 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        StringBuilder sb = new StringBuilder();
 
         int N = Integer.parseInt(st.nextToken());   //보석 수
-        int bagNum = Integer.parseInt(st.nextToken());   //가방 수
+        int K = Integer.parseInt(st.nextToken());   //가방 수
 
+        // 보석 입력받아 무게 가벼운 순으로 정렬
         PriorityQueue<Gem> gems = new PriorityQueue<>();
         for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
@@ -48,20 +48,23 @@ public class Main {
             int v = Integer.parseInt(st.nextToken());
             gems.offer(new Gem(m, v));
         }
-        int[] bags = new int[bagNum];
-        for (int i = 0; i < bagNum; i++) {
+        
+        // 가방 입력, 작은 순으로 정렬
+        int[] bags = new int[K];
+        for (int i = 0; i < K; i++) {
             bags[i] = Integer.parseInt(br.readLine());
         }
         Arrays.sort(bags);
 
         long answer = 0;
-        PriorityQueue<Integer> cost = new PriorityQueue<>();
-        for (int bag : bags) {
-            while (!gems.isEmpty()&&gems.peek().getM()<=bag){
-                cost.offer(gems.poll().getCost()*-1);
+        // 가방에 넣을 수 있는 보석 후보들의 가격(내림차순)
+        PriorityQueue<Integer> cost = new PriorityQueue<>(Comparator.reverseOrder());
+        for (int bag : bags) {  // 작은 가방부터
+            while (!gems.isEmpty() && gems.peek().getM() <= bag) {  // 지금 가방에 들어갈 수 있는 보석들 전부 가격 후보에 넣기
+                cost.offer(gems.poll().getCost());
             }
-            if(!cost.isEmpty()) {
-                answer += cost.poll()*(-1);
+            if (!cost.isEmpty()) {  // 후보 중 가장 가치 높은 보석 답에 넣기
+                answer += cost.poll();
             }
         }
         System.out.println(answer);
