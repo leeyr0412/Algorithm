f9e91a2ea077204c22db88fe43e830818689457d
Yeri Lee <leeyr0412@naver.com>
2025-01-13 19:18:53 +0900

    [Gold IV] Title: 마법사 상어와 파이어볼, Time: 356 ms, Memory: 33760 KB -BaekjoonHub

--- 백준/Gold/20056. 마법사 상어와 파이어볼/마법사 상어와 파이어볼.java
+++ 백준/Gold/20056. 마법사 상어와 파이어볼/마법사 상어와 파이어볼.java
@@ -1,169 +1,105 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayDeque;
-import java.util.ArrayList;
-import java.util.Deque;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
-/**
- * 마법사 상어와 파이어볼 / 골드4 / 1시간 30분  / 3월 6일 
- * https://www.acmicpc.net/problem/20056
- */
-class FireBall {
-    private int r, c, m, d, speed;
+class Fireball {
+    int r, c, m, d, s;
 
-    public FireBall(int r, int c, int m, int d, int speed) {
+    public Fireball(int r, int c, int m, int d, int s) {
         this.r = r;
         this.c = c;
         this.m = m;
         this.d = d;
-        this.speed = speed;
-    }
-
-    public int getR() {
-        return r;
-    }
-
-    public int getC() {
-        return c;
-    }
-
-    public int getM() {
-        return m;
-    }
-
-    public int getD() {
-        return d;
-    }
-
-    public int getSpeed() {
-        return speed;
-    }
-}
-
-class Pos {
-    private int r, c;
-    private FireBall ball;
-
-    public Pos(int r, int c, FireBall ball) {
-        this.r = r;
-        this.c = c;
-        this.ball = ball;
-    }
-
-    public int getR() {
-        return r;
-    }
-
-    public int getC() {
-        return c;
-    }
-
-    public FireBall getBall() {
-        return ball;
+        this.s = s;
     }
 }
 
 public class Main {
-	static int[][] dr = {{-1, 0}, {-1, 1}, {0, 1}, {1, 1}, {1, 0}, {1, -1}, {0, -1}, {-1, -1}};
-    static int N;
-    static ArrayList<FireBall>[][] map;
-    static Deque<Pos> ballList = new ArrayDeque<>();
+    static int[][] dirs = {
+        {-1, 0}, {-1, 1}, {0, 1}, {1, 1},
+        {1, 0}, {1, -1}, {0, -1}, {-1, -1}
+    };
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        N = Integer.parseInt(st.nextToken());
-        map = new ArrayList[N + 1][N + 1];
-        for(int r = 1; r <N+1;r++){
-            for(int c = 1; c<N+1; c++){
-                map[r][c] = new ArrayList<>();
-            }
-        }
-        int ballNum = Integer.parseInt(st.nextToken());
-        int commandNum = Integer.parseInt(st.nextToken());
-        for (int i = 0; i < ballNum; i++) {
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+
+        List<Fireball>[][] map = new ArrayList[N][N];
+        for (int i = 0; i < N; i++)
+            for (int j = 0; j < N; j++)
+                map[i][j] = new ArrayList<>();
+
+        List<Fireball> fireballs = new ArrayList<>();
+        for (int i = 0; i < M; i++) {
             st = new StringTokenizer(br.readLine());
-            int r = Integer.parseInt(st.nextToken());
-            int c = Integer.parseInt(st.nextToken());
+            int r = Integer.parseInt(st.nextToken()) - 1;
+            int c = Integer.parseInt(st.nextToken()) - 1;
             int m = Integer.parseInt(st.nextToken());
-            int speed = Integer.parseInt(st.nextToken());
+            int s = Integer.parseInt(st.nextToken());
             int d = Integer.parseInt(st.nextToken());
-            ballList.add(new Pos(r, c, new FireBall(r, c, m, d, speed)));
+            fireballs.add(new Fireball(r, c, m, d, s));
         }
 
-        for (int command = 0; command < commandNum; command++) {
-            //이동
-            for (Pos pos : ballList) {
-                int d = pos.getBall().getD();
-                int speed = pos.getBall().getSpeed();
-                int newR = pos.getR() + dr[d][0] * speed;
-                if (newR == 0 || newR % N == 0) {
-                    newR = N;
-                } else if (newR < 0) {
-                    newR = newR % N + N;
-                } else {
-                    newR = newR % N;
-                }
-                int newC = pos.getC() + dr[d][1] * speed;
-                if (newC == 0 || newC % N == 0) {
-                    newC = N;
-                } else if (newC < 0) {
-                    newC = newC % N + N;
-                } else {
-                    newC = newC % N;
-                }
-                map[newR][newC].add(new FireBall(newR, newC, pos.getBall().getM(), d, speed));
+        while (K-- > 0) {
+            // 이동
+            for (Fireball f : fireballs) {
+                int moveR = (f.r + dirs[f.d][0] * f.s % N + N) % N;
+                int moveC = (f.c + dirs[f.d][1] * f.s % N + N) % N;
+                f.r = moveR;
+                f.c = moveC;
+                map[moveR][moveC].add(f);
             }
-            ballList = new ArrayDeque<>();
-            //합치기
-            for (int r = 1; r <= N; r++) {
-                for (int c = 1; c <= N; c++) {
-                    if (map[r][c].size() > 1) {
-                        int even = 0;
-                        int odd = 0;
-                        int M = 0;
-                        int speed = 0;
-                        for (FireBall ball : map[r][c]) {
-                            M += ball.getM();
-                            speed += ball.getSpeed();
-                            int d = ball.getD() % 2;
-                            if(d ==0) {
-                            	even++;
-                            }else {
-                            	odd++;
-                            }
+
+            List<Fireball> next = new ArrayList<>();
+
+            // 합치기
+            for (int r = 0; r < N; r++) {
+                for (int c = 0; c < N; c++) {
+                    List<Fireball> cell = map[r][c];
+                    int size = cell.size();
+                    if (size == 0) continue;
+
+                    if (size == 1) {
+                        next.add(cell.get(0));
+                    } else {
+                        int sumM = 0, sumS = 0;
+                        boolean allEven = true, allOdd = true;
+
+                        for (Fireball f : cell) {
+                            sumM += f.m;
+                            sumS += f.s;
+                            if (f.d % 2 == 0) allOdd = false;
+                            else allEven = false;
                         }
-                        if (M / 5 > 0) {
-                            if (even*odd == 0) { //방향 같음
-                                ballList.add(new Pos(r, c, new FireBall(r, c, M / 5, 0, speed / map[r][c].size())));
-                                ballList.add(new Pos(r, c, new FireBall(r, c, M / 5, 2, speed / map[r][c].size())));
-                                ballList.add(new Pos(r, c, new FireBall(r, c, M / 5, 4, speed / map[r][c].size())));
-                                ballList.add(new Pos(r, c, new FireBall(r, c, M / 5, 6, speed / map[r][c].size())));
-                            } else {
-                                ballList.add(new Pos(r, c, new FireBall(r, c, M / 5, 1, speed / map[r][c].size())));
-                                ballList.add(new Pos(r, c, new FireBall(r, c, M / 5, 3, speed / map[r][c].size())));
-                                ballList.add(new Pos(r, c, new FireBall(r, c, M / 5, 5, speed / map[r][c].size())));
-                                ballList.add(new Pos(r, c, new FireBall(r, c, M / 5, 7, speed / map[r][c].size())));
-                            }
-                        } else {  //질량 0이면 사라짐
-                            map[r][c] = new ArrayList<>();
+
+                        int newM = sumM / 5;
+                        if (newM == 0) {
+                            cell.clear();
+                            continue;
                         }
-                        map[r][c] = new ArrayList<>();
-                    } else if (map[r][c].size() == 1) {
-                        ballList.add(new Pos(r, c, map[r][c].get(0)));
-                        map[r][c].remove(0);
+
+                        int newS = sumS / size;
+                        int[] newDirs = (allEven || allOdd) ? new int[]{0, 2, 4, 6} : new int[]{1, 3, 5, 7};
+
+                        for (int d : newDirs) {
+                            next.add(new Fireball(r, c, newM, d, newS));
+                        }
                     }
+                    cell.clear(); // 꼭 clear 해줘야 메모리 절약됨
                 }
             }
-        }       //명령 끝
 
-        int answer = 0;
-        for (Pos pos : ballList) {
-            answer+= pos.getBall().getM();
+            fireballs = next; // 다음 턴으로 이동
         }
-        System.out.println(answer);
+
+        int totalMass = 0;
+        for (Fireball f : fireballs) {
+            totalMass += f.m;
+        }
+
+        System.out.println(totalMass);
     }
 }
