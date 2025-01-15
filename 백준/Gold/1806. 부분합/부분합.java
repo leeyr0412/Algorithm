de4f7fb605bf1ceffca09e6ddcda8a501f85b41a
Yeri Lee <leeyr0412@naver.com>
2025-01-15 19:18:53 +0900

    [Gold IV] Title: 부분합, Time: 240 ms, Memory: 23908 KB -BaekjoonHub

--- 백준/Gold/1806. 부분합/부분합.java
+++ 백준/Gold/1806. 부분합/부분합.java
@@ -1,43 +1,38 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        //StringBuilder sb = new StringBuilder();
-        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringTokenizer st;
 
+        st = new StringTokenizer(br.readLine());
         int N = Integer.parseInt(st.nextToken());
-        int M = Integer.parseInt(st.nextToken());
-        int[] num = new int[N];
+        int S = Integer.parseInt(st.nextToken());
+        int[] arr = new int[N];
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < N; i++) {
-            num[i] = Integer.parseInt(st.nextToken());
+            arr[i] = Integer.parseInt(st.nextToken());
         }
-
-        int answer = N + 1;
-        int sum = num[0];
         int start = 0;
-        int end = 0;
-        while (start < N) {
-            int length = 1;
-            if (sum < M && end < N - 1) {
-                end++;
-                sum += num[end];
-            } else if (sum >= M) {
-                length += end - start;
-                answer = Math.min(answer, length);
-                sum -= num[start];
+        int sum = 0;
+        int answer = N + 1;
+
+        // end 포인터가 배열의 끝까지 이동
+        for (int end = 0; end < N; end++) {
+            sum += arr[end];
+            while (sum >= S) {
+                answer = Math.min(answer, end - start + 1);
+                sum -= arr[start];
                 start++;
-            } else {
-                break;
             }
         }
-        if (answer > N) {
-            answer = 0;
+        if (answer == N + 1) {
+            System.out.println(0);
+        } else {
+            System.out.println(answer);
         }
-        System.out.println(answer);
     }
 }
