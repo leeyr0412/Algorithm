96e3dd512ff3d94e6c9a1468322e108e813c6f75
Yeri Lee <leeyr0412@naver.com>
2025-02-19 19:18:53 +0900

    [Gold III] Title: 아기 상어, Time: 112 ms, Memory: 14784 KB -BaekjoonHub

--- 백준/Gold/16236. 아기 상어/아기 상어.java
+++ 백준/Gold/16236. 아기 상어/아기 상어.java
@@ -1,176 +1,127 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayDeque;
-import java.util.Deque;
-import java.util.PriorityQueue;
-import java.util.StringTokenizer;
+import java.util.*;
 
-class Pos implements Comparable<Pos> {
-	int r, c, count;
+class Point implements Comparable<Point> {
+    private int r, c, count;
 
-	public Pos(int r, int c, int count) {
-		super();
-		this.r = r;
-		this.c = c;
-		this.count = count;
-	}
+    public Point(int r, int c, int count) {
+        this.r = r;
+        this.c = c;
+        this.count = count;
+    }
 
-	public int getR() {
-		return r;
-	}
+    public int getR() {
+        return r;
+    }
 
-	public void setR(int r) {
-		this.r = r;
-	}
+    public int getC() {
+        return c;
+    }
 
-	public int getC() {
-		return c;
-	}
+    public int getCount() {
+        return count;
+    }
 
-	public void setC(int c) {
-		this.c = c;
-	}
-
-	public int getCount() {
-		return count;
-	}
-
-	public void setCount(int count) {
-		this.count = count;
-	}
-
-	@Override
-	public int compareTo(Pos o) {
-		if (this.count > o.count) {
-			return 1;
-		} else if (this.count == o.count) {
-			if (this.r > o.r) {
-				return 1;
-			} else if (this.r == o.r) {
-				return this.c - o.c;
-			} else {
-				return -1;
-			}
-		}
-		return -1;
-	}
-
+    @Override
+    public int compareTo(Point o) {
+        if (this.count == o.count) {
+            if (this.r == o.r) {
+                return this.c - o.c;
+            }
+            return this.r - o.r;
+        }
+        return this.count - o.count;
+    }
 }
 
 public class Main {
+    static int nowSize, time, eatCount;
+    static int[][] map;
+    static int N;
 
-	static int[][] map;
-	static int[][] dr = { { -1, 0 }, { 1, 0 }, { 0, 1 }, { 0, -1 } };
-	static int N;
-	static int[] curr = new int[2]; // 상어 위치
-	static int currSize = 2;// 상어 크기
-	static int eatCount = 0;// 상어 크기
-//if (newR < R && newR >= 0 && newC < C && newC >= 0) {// 범위검사
-	/*
-	 * int newR = r + d[0]; int newC = c + d[1];
-	 */
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st; // = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(br.readLine());
+        map = new int[N][N];
+        int[] now = {0, 0};
+        for (int r = 0; r < N; r++) {
+            st = new StringTokenizer(br.readLine());
+            for (int c = 0; c < N; c++) {
+                map[r][c] = Integer.parseInt(st.nextToken());
+                if (map[r][c] == 9) {
+                    now[0] = r;
+                    now[1] = c;
+                    map[r][c] = 0;
+                }
+            }
+        }
 
-		int answer = 0;
-		N = Integer.parseInt(br.readLine());
-		map = new int[N][N];
-		for (int r = 0; r < N; r++) {
-			st = new StringTokenizer(br.readLine());
-			for (int c = 0; c < N; c++) {
-				map[r][c] = Integer.parseInt(st.nextToken());
-				if (map[r][c] == 9) {
-					curr[0] = r;
-					curr[1] = c;
-					map[r][c] = 0;
-				}
-			}
-		}
-		while (true) {
-			// 먹을 수 있는 물고기 있는지 검사
-//			if (help()) {
-//				break;
-//			}
+        nowSize = 2;
+        time = 0;
+        eatCount = 0;
+        while ((now = getNextPoint(now)) != null) {     // 이동 가능한 좌표가 있으면 반복
+            eatCount++; //물고기 먹기
+            map[now[0]][now[1]] = 0;
+            if (eatCount == nowSize) {  // 사이즈 만큼 먹었을 경우 크기 자람
+                nowSize++;
+                eatCount = 0;
+            }
+        }
+        System.out.println(time);
+    }
 
-			// 물고기 먹으러 감
-			int time = findFish();
+    /**
+     * 다음 이동할 좌표(먹어야하는 물고기가 있는 위치)를 구하는 함수
+     *
+     * @param now 현재 좌표
+     * @return 다음 이동 좌표
+     */
+    private static int[] getNextPoint(int[] now) {
+        boolean[][] visited = new boolean[N][N];
+        // 거리순, 거리가 같다면 위쪽, 왼쪽 우선순위를 위해 우선순위 큐 사용
+        PriorityQueue<Point> que = new PriorityQueue<>();
+        que.add(new Point(now[0], now[1], 0));
+        visited[now[0]][now[1]] = true;
+        int[][] dirs = {{-1, 0}, {0, -1}, {0, 1}, {1, 0}};
+        while (!que.isEmpty()) {
+            Point nowPoint = que.poll();
 
-//			print();
+            // 잡아 먹을 수 있는 물고기가 있는 좌표면 좌표 반환
+            if (map[nowPoint.getR()][nowPoint.getC()] > 0
+                    && map[nowPoint.getR()][nowPoint.getC()] < nowSize) {
+                time += nowPoint.getCount();    // 물고기까지 거리(이동시간) 더하기
+                return new int[]{nowPoint.getR(), nowPoint.getC()};
+            }
+            for (int[] dir : dirs) {
+                int nextR = nowPoint.getR() + dir[0];
+                int nextC = nowPoint.getC() + dir[1];
+                if (canMove(nextR, nextC) && !visited[nextR][nextC]) {    // 이동 가능하면 큐에 삽입
+                    que.add(new Point(nextR, nextC, nowPoint.getCount() + 1));
+                    visited[nextR][nextC] = true;
+                }
+            }
+        }
+        return null;
+    }
 
-			if (time < 0)
-				break;
-			answer += time;
-			if (eatCount == currSize) {
-				currSize++;
-				eatCount = 0;
-			}
-		}
-
-		System.out.println(answer);
-	}
-
-//	private static void print() {
-//		System.out.println("==============================================");
-//		for (int r = 0; r < N; r++) {
-//			for (int c = 0; c < N; c++) {
-//				System.out.printf("%d ",map[r][c]);
-//			}
-//			System.out.println();
-//		}
-//	}
-
-	private static int findFish() {
-		boolean[][] visited = new boolean[N][N];
-		PriorityQueue<Pos> que = new PriorityQueue<>();
-		que.add(new Pos(curr[0], curr[1], 0));
-		visited[curr[0]][curr[1]] = true;
-		while (!que.isEmpty()) {
-			Pos now = que.poll();
-			if (map[now.r][now.c] > 0) {
-				if (map[now.r][now.c] < currSize) { // 먹을 수 있음
-					eatCount++; // 먹은 물고기 수 증가
-					curr[0] = now.r; // 현재 좌표 이동
-					curr[1] = now.c;
-					map[now.r][now.c] = 0; // 물고기 먹었음
-					return now.count;
-				}
-			}
-			for (int[] d : dr) {
-				int newR = now.r + d[0];
-				int newC = now.c + d[1];
-				if (newR < N && newR >= 0 && newC < N && newC >= 0) {// 범위검사
-					if (map[newR][newC] > currSize || visited[newR][newC]) {
-						continue;
-					}
-					visited[newR][newC] = true;
-					que.add(new Pos(newR, newC, now.count + 1));
-				}
-			}
-		}
-
-		return -1;
-	}
+    /**
+     * 이동 가능한지 확인(범위 내 빈 곳이거나, 작거나 같은 사이즈의 물고기가 있는 곳)
+     *
+     * @param nextR
+     * @param nextC
+     * @return
+     */
+    private static boolean canMove(int nextR, int nextC) {
+        if (nextR < 0 || nextR >= N || nextC < 0 || nextC >= N
+                || map[nextR][nextC] > nowSize) {
+            return false;
+        }
+        return true;
+    }
 
 }
-
-/**
- * 먹을 수 있는 물고기 있으면 false 아니면 도움청함
- * 
- * @return
- */
-//	private static boolean help() {
-//		for(int r = 0; r < N; r++) {
-//			for (int c = 0; c < N; c++) {
-//				if(map[r][c]>0) {
-//					if(map[r][c] <currSize) {
-//						return false;
-//					}
-//				}
-//			}
-//		}
-//		return true;
-//	}
-//}
