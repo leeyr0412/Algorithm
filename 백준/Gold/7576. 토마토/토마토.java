8f54eee8eafb0862f74dd21d0867d6a6b98c411e
Yeri Lee <leeyr0412@naver.com>
2025-02-20 19:18:53 +0900

    [Gold V] Title: 토마토, Time: 544 ms, Memory: 103384 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.java
+++ 백준/Gold/7576. 토마토/토마토.java
@@ -1,59 +1,85 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayDeque;
-import java.util.Deque;
-import java.util.StringTokenizer;
+import java.util.*;
+
+class Tomato {
+    private int r;
+    private int c;
+    private int count;
+
+    public Tomato(int r, int c, int count) {
+        this.r = r;
+        this.c = c;
+        this.count = count;
+    }
+
+    public int getR() {
+        return r;
+    }
+
+    public int getC() {
+        return c;
+    }
+
+    public int getCount() {
+        return count;
+    }
+}
 
 public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
 
-	static int[][] map;
+        st = new StringTokenizer(br.readLine());
+        int M = Integer.parseInt(st.nextToken());
+        int N = Integer.parseInt(st.nextToken());
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		int C = Integer.parseInt(st.nextToken());
-		int R = Integer.parseInt(st.nextToken());
-		map = new int[R][C];
-		Deque<int[]> que = new ArrayDeque<int[]>();
-		for (int r = 0; r < R; r++) {
-			st = new StringTokenizer(br.readLine());
-			for (int c = 0; c < C; c++) {
-				map[r][c] = Integer.parseInt(st.nextToken());
-				if (map[r][c] == 1) {
-					que.add(new int[] { r, c });
-				}
-			}
-		}
+        // 토마토 상태 및 방문 배열
+        int[][] box = new int[N][M];
+        // 익지 않은 토마토 수
+        int remain = 0;
 
-		int[][] dr = { { -1, 0 }, { 1, 0 }, { 0, 1 }, { 0, -1 } };
+        Deque<Tomato> que = new ArrayDeque<>();
+        for (int r = 0; r < N; r++) {
+            st = new StringTokenizer(br.readLine());
+            for (int c = 0; c < M; c++) {
+                box[r][c] = Integer.parseInt(st.nextToken());
+                if (box[r][c] == 1) {   // 익은 토마토는 바로 큐에 삽입
+                    que.add(new Tomato(r, c, 0));
+                } else if (box[r][c] == 0) { // 익지 않은 토마토 수를 카운트
+                    remain++;
+                }
+            }
+        }
 
-		int max = 1;
-		while (!que.isEmpty()) {
-			int[] pos = que.poll();
-
-			for (int[] d : dr) {
-				int nr = pos[0] + d[0];
-				int nc = pos[1] + d[1];
-				if (nr < R && nr >= 0 && nc < C && nc >= 0) {
-					if (map[nr][nc] == 0) {
-						map[nr][nc] = map[pos[0]][pos[1]] + 1;
-						max = Math.max(max, map[nr][nc]);
-						que.add(new int[] { nr, nc });
-					}
-				}
-			}
-		}
-		
-		for (int r = 0; r < R; r++) {
-			for (int c = 0; c < C; c++) {
-				if (map[r][c] == 0) {
-					System.out.println(-1);
-					return;
-				}
-			}
-		}
-		System.out.println(max-1);
-	}
-
+        int[][] dirs = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+        int answer = 0;
+        // BFS 탐색
+        while (!que.isEmpty()) {
+            Tomato now = que.pop();
+            answer = Math.max(answer, now.getCount());
+            for (int[] dir : dirs) {
+                int nextR = now.getR() + dir[0];
+                int nextC = now.getC() + dir[1];
+                if (nextR < 0 || nextR >= N || nextC < 0 || nextC >= M) {
+                    continue;
+                }
+                if (box[nextR][nextC] == 0) {
+                    box[nextR][nextC] = 1;
+                    remain--; // 익지 않은 토마토 수 감소
+                    que.add(new Tomato(nextR, nextC, now.getCount() + 1));
+                }
+            }
+        }
+        
+        // 남은 토마토가 없을 때 정답 출력
+        if (remain == 0) {
+            System.out.println(answer);
+        } else {
+            System.out.println(-1);
+        }
+    }
 }
