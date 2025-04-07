5ac1a7a2f4e86e8ac8b8b0f9ecd003e75179772e
Yeri Lee <leeyr0412@naver.com>
2025-04-07 19:18:53 +0900

    [Gold III] Title: 줄 세우기, Time: 408 ms, Memory: 44132 KB -BaekjoonHub

--- 백준/Gold/2252. 줄 세우기/줄 세우기.java
+++ 백준/Gold/2252. 줄 세우기/줄 세우기.java
@@ -3,46 +3,50 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
 
-/**
-* 줄세우기 / 난이도 / 걸린시간 / 3월21일
-*/
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-
-        int v = Integer.parseInt(st.nextToken());
-        int e = Integer.parseInt(st.nextToken());
-        int [] indegree = new int[v+1];
-        List<Integer>[]graph = new List[v+1];
-        for(int i = 1; i <= v; i++){
-            graph[i] = new ArrayList<Integer>();
-        }
-        for(int i = 0; i < e; i++){
-            st = new StringTokenizer(br.readLine());
-            int from = Integer.parseInt(st.nextToken());
-            int to = Integer.parseInt(st.nextToken());
-            graph[from].add(to);
-            indegree[to]++;
-        }
         StringBuilder sb = new StringBuilder();
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        List<Integer>[] graph = new List[N + 1];
+        int[] inDegree = new int[N + 1];    // 진입 차수
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int front = Integer.parseInt(st.nextToken());
+            int back = Integer.parseInt(st.nextToken());
+            inDegree[back]++;
+            if (graph[front] == null) {
+                graph[front] = new ArrayList<>();
+            }
+            graph[front].add(back);
+        }
+
         Queue<Integer> que = new ArrayDeque<>();
-        for (int i =1; i <=v;i++ ) {
-            //진입 차수 = 0
-            if(indegree[i]==0){
+        for (int i = 1; i <= N; i++) {
+            if (inDegree[i] == 0) {
                 que.add(i);
             }
         }
-        while (!que.isEmpty()){
+
+        while (!que.isEmpty()) {
             int now = que.poll();
             sb.append(now).append(" ");
-            for (Integer to : graph[now]) {
-                indegree[to]--;
-                if(indegree[to]==0){
-                    que.add(to);
+
+            if (graph[now] == null) {
+                continue;
+            }
+            for (Integer next : graph[now]) {
+                inDegree[next]--;
+                if (inDegree[next] == 0) {
+                    que.add(next);
                 }
             }
         }
-        System.out.println(sb);
+
+        System.out.println(sb.toString());
     }
 }
